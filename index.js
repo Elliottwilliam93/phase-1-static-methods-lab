504cc47799a8a3d08910a911843733f7b4c56fe4
williamelliott <ironmaiden229@hotmail.com>
2023-05-05 16:23:54 -0400

    project done

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 class Formatter {
-  //add static methods here
+    static capitalize(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1)
+    }
+    static sanitize(string) {
+      return string.replace(/[^A-Za-z0-9-'\s]/g,"");
+    }
+    static titleize(string) {
+      const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+      const words = string.toLowerCase().split(' ');
+      const capitalizedWords = words.map((word, index) => {
+        if (index === 0 || !exceptions.includes(word)) {
+          return word.charAt(0).toUpperCase() + word.slice(1);
+        } else {
+          return word;
+        }
+      });
+      return capitalizedWords.join(' ');
+    }
 }
